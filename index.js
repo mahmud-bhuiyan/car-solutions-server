2c9739f7e802edba16e25a2da721bb4381e618cf
mahmud-bhuiyan <bh.mahmud@gmail.com>
2023-05-17 00:56:47 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+var cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5nrgbhc.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged! Connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Car Solutions is ready to serve..");
+});
+
+app.listen(port, () => {
+  console.log(`Car Solutions server is running on port ${port}`);
+});
